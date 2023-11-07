de35fc6f7f228c5cd98c8719caabf803a026c6e0
Dominic Talbot <Dominictalbot@live.co.uk>
2023-11-07 15:20:12 -0500

    Cleaned up the code

--- BrakehillBarnsBooking/wwwroot/js/site.js
+++ BrakehillBarnsBooking/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Get the current date in ISO format (YYYY-MM-DD)
+const currentDate = new Date().toISOString().split('T')[0];
+// Set the min attribute of the CheckIn input field to prevent selection of past dates
+document.getElementById("CheckIn").min = currentDate;
 
-// Write your JavaScript code.
+// Add an event listener to the CheckIn input to dynamically set the min attribute of CheckOut input
+document.getElementById("CheckIn").addEventListener("change", function () {
+    const checkInDate = new Date(this.value);
+    const minCheckOutDate = new Date(checkInDate);
+    minCheckOutDate.setDate(checkInDate.getDate() + 1); // Minimum CheckOut date is the day after CheckIn
+    const minCheckOutDateISO = minCheckOutDate.toISOString().split('T')[0];
+    document.getElementById("CheckOut").min = minCheckOutDateISO;
+
+    // Reset the CheckOut date when a new CheckIn date is chosen
+    document.getElementById("CheckOut").value = "";
+});
